573c12631977977b85bfdb1b7d856d7c6fc1c406
Kamil Górkowy <56930464+KGorkowy@users.noreply.github.com>
2025-10-28 20:47:24 +0100

    Second version of gauge component added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,45 @@ function App() {
     return (
         <div className="App">
             <div className="camera">
-                <DriverMonitoring />
+                <DriverMonitoring/>
             </div>
-            <div className="gauge" style={{ marginTop: '20px', maxWidth: '1020px' }}>
+            <div className="gauge" style={{marginTop: '20px'}}>
                 <GaugeComponent
-                    size={200}
-                    value={10}
-                    minValue={0}
-                    maxValue={100}
-                    label="Driver Alertness"
-                    color="#FF0000"
-
+                    id="gauge-component2"
+                    value={80}
+                    pointer={{type: "arrow", elastic: true}}
+                    range={{min: 0, max: 100}}
+                    width={300}
+                    height={200}
+                    arc={{
+                        width: 0.3,
+                        subArcs: [
+                            {
+                                limit: 50,
+                                color: '#00FF00',
+                                showTick: true
+                            },
+                            {
+                                limit: 75,
+                                color: '#FFFF00',
+                                showTick: true
+                            },
+                            {
+                                limit: 100,
+                                color: '#FF0000'
+                            }
+                        ]
+                    }}
+                    // Remove the deprecated/incorrect properties for this configuration
+                    // segments={10}
+                    // arcWidth={0.3}
+                    // colors={['#FF0000', '#FFFF00', '#00FF00']}
+                    needleColor={"#000000"}
+                    textColor={"#000000"}
                 />
             </div>
         </div>
     );
 }
+
 export default App;
