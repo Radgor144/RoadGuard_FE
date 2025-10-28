6f09f92820c91f15a12d16816d35210ccb0e5ced
KGorkowy <k.gorkowy@op.pl>
2025-10-28 20:11:19 +0100

    First version of gauge component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import DriverMonitoring from './mediapipe/components/DriverMonitoring';
 import './App.css';
+import DriverMonitoring from "./mediapipe/components/DriverMonitoring";
+import {GaugeComponent} from "react-gauge-component";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <DriverMonitoring />
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <div className="camera">
+                <DriverMonitoring />
+            </div>
+            <div className="gauge" style={{ marginTop: '20px', maxWidth: '1020px' }}>
+                <GaugeComponent
+                    size={200}
+                    value={10}
+                    minValue={0}
+                    maxValue={100}
+                    label="Driver Alertness"
+                    color="#FF0000"
 
+                />
+            </div>
+        </div>
+    );
+}
 export default App;
